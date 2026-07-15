cdb269275a273e3ff97f7b0134602fac26a7612e
Ferdinand Tonby-Strandborg <ferdinand.tonby.strandborg@gmail.com>
2026-07-15 13:36:47 +0800

    Now gestures reading from json

--- scripts/gestures.py
+++ scripts/gestures.py
@@ -2,23 +2,26 @@
 """Subscribes to the hand pose message, to classify gestures based on them."""
 import numpy as np
 
+import rospkg
 import rospy
 from std_msgs.msg import String
 from person_follower_msgs.msg import PoseArray
+from visualization_msgs.msg import MarkerArray, Marker
 
 from collections import deque
 from dataclasses import dataclass, field
 from enum import Enum, auto
 from typing import Dict, Optional
 import math
+import json
 
 ################
 ### SETTINGS ###
 ################
 # Values in degrees
 BUFFER = 20
-OPEN_THRESHOLD = 140
-FOLDED_THRESHOLD = 100
+OPEN_THRESHOLD = 130
+FOLDED_THRESHOLD = 90
 
 # Frames, meters
 MOTION_WINDOW = 8
@@ -33,6 +36,14 @@ INDEX_MCP,  INDEX_PIP,  INDEX_DIP,  INDEX_TIP  = 5,  6,  7,  8
 MIDDLE_MCP, MIDDLE_PIP, MIDDLE_DIP, MIDDLE_TIP = 9,  10, 11, 12
 RING_MCP,   RING_PIP,   RING_DIP,   RING_TIP   = 13, 14, 15, 16
 PINKY_MCP,  PINKY_PIP,  PINKY_DIP,  PINKY_TIP  = 17, 18, 19, 20
+
+FINGER_IDXS = {
+    "thumb":  (WRIST,      THUMB_CMC,  THUMB_MCP,  THUMB_IP,   THUMB_TIP),
+    "index":  (INDEX_MCP,  INDEX_PIP,  INDEX_DIP,  INDEX_TIP),
+    "middle": (MIDDLE_MCP, MIDDLE_PIP, MIDDLE_DIP, MIDDLE_TIP),
+    "ring":   (RING_MCP,   RING_PIP,   RING_DIP,   RING_TIP),
+    "pinky":  (PINKY_MCP,  PINKY_PIP,  PINKY_DIP,  PINKY_TIP)
+}
 
 FINGER_JOINTS = {
     "thumb":  (0,  3,  4), # (2,  3,  4),
@@ -223,6 +234,71 @@ class GestureTemplate:
 
         return True
 
+    @staticmethod
+    def from_json(filename):
+        with open(filename, 'r', encoding='utf-8') as file:
+            return GestureTemplate.from_config(json.load(file))
+
+    @staticmethod
+    def from_config(values):
+        if isinstance(values, dict):
+            names = list(values.keys())
+            values = [values[n] for n in names]
+            for i, n in enumerate(names):
+                values[i]["name"] = n
+
+        templates = []
+        for v in values:
+            templates.append(GestureTemplate(
+                name = v["name"],
+                hand = v.get("hand", "either"),
+                finger_states = GestureTemplate.parse_fingers(v["finger_states"]),
+                palm_dir      = GestureTemplate.parse_direction(v.get("palm_dir")),
+                thumb_dir     = GestureTemplate.parse_direction(v.get("thumb_dir")),
+                knuckle_dir   = GestureTemplate.parse_direction(v.get("knuckle_dir")),
+                motion        = GestureTemplate.parse_direction(v.get("motion"))
+            ))
+
+        return templates
+
+    @staticmethod
+    def parse_fingers(fingers):
+        finger_states = {}
+        for k, v in fingers.items():
+            if v.lower() in ["open", "o"]:
+                finger_states[k] = FingerState.OPEN
+            elif v.lower() in ["folded", "f"]:
+                finger_states[k] = FingerState.FOLDED
+            else:
+                raise ValueError(f"Unrecognized finger state '{v}' for finger '{k}'!")
+        return finger_states
+
+    @staticmethod
+    def parse_direction(direction):
+        if direction is None:
+            return None
+
+        direction = direction.lower()
+        if direction in ["towards", "t"]:
+            return Direction.TOWARDS
+        if direction in ["away", "a"]:
+            return Direction.AWAY
+        if direction in ["up", "u"]:
+            return Direction.UP
+        if direction in ["down", "d"]:
+            return Direction.DOWN
+        if direction in ["left", "l"]:
+            return Direction.LEFT
+        if direction in ["right", "r"]:
+            return Direction.RIGHT
+        if direction in ["static", "s"]:
+            return Direction.STATIC
+        if direction is None:
+            return None
+        raise ValueError(f"Unrecognized direction '{direction}'!")
+
+        
+
 ###########################
 ### SLIDING WINDOW VOTE ###
 ###########################
@@ -264,6 +340,7 @@ class GestureNode:
         self.last_published = None
 
         self.gesture_pub = rospy.Publisher("/person_follower/gesture", String, queue_size=1)
+        self.vis_pub = rospy.Publisher("/person_follower/gesture/visualization", MarkerArray, queue_size=1)
 
         self.hands_sub = rospy.Subscriber("/person_follower/hands", PoseArray, self.hands_callback, queue_size=5)
 
@@ -276,7 +353,7 @@ class GestureNode:
         return self.none_gesture
 
     def hands_callback(self, msg):
-        smoothed, motions = {}, {}
+        smoothed, motions, poses = {}, {}, {}
         for pose in msg.poses:
             is_right = pose.score > 0.5
             side = "right" if is_right else "left"
@@ -290,13 +367,96 @@ class GestureNode:
             raw = build_hand_sample(pose, prev_filtered)
             smoothed[side] = self.hand_filters[side].update(raw)
             motions[side]  = self.motion_filters[side].update(np.array([pose.pose.position.x, pose.pose.position.y, pose.pose.position.z]))
+            poses[side] = pose
 
         raw_gesture = self.classify(smoothed, motions)
         voted_gesture = self.vote_filter.update(raw_gesture)
 
         self.gesture_pub.publish(String(data=voted_gesture))
         self.last_published = voted_gesture
+
+        self.visualize(smoothed, poses, voted_gesture, msg.header)
         # rospy.loginfo("Gesture '%s'", voted_gesture)
+
+
+    def visualize(self, smoothed, poses, voted_gesture, header):
+        if not self.vis_pub.get_num_connections():
+            return
+
+        marker_array = MarkerArray()
+        for i, key in enumerate(smoothed):
+            state = smoothed[key].finger_states
+            pose = poses[key]
+
+            for j, pt in enumerate(pose.keypoints):
+                marker = Marker()
+                marker.header = header
+                marker.lifetime = rospy.Duration(0.5) 
+
+                marker.id = i * (21 + 1) + j
+                marker.type = Marker.SPHERE
+                marker.action = Marker.ADD
+
+                marker.pose.position.x = pt.x + 0.4 * (1 if key == 'right' else -1)
+                marker.pose.position.y = pt.y
+                marker.pose.position.z = pt.z
+                
+                marker.pose.orientation.x = 0
+                marker.pose.orientation.y = 0
+                marker.pose.orientation.z = 0
+                marker.pose.orientation.w = 1
+
+                marker.scale.x = 0.02
+                marker.scale.y = 0.02
+                marker.scale.z = 0.02
+
+                marker.color.a = 1.0
+
+                for f, idxs in FINGER_IDXS.items():
+                    if j in idxs:
+                        finger = f
+                        break
+                else:
+                    finger = "thumb"
+
+                if state[finger] == FingerState.FOLDED:
+                    marker.color.r = 0.8
+                elif state[finger] == FingerState.OPEN:
+                    marker.color.g = 0.8
+                else:
+                    marker.color.b = 0.8
+
+                marker_array.markers.append(marker)
+
+            label = Marker()
+            label.header = header
+            label.lifetime = rospy.Duration(0.5)
+
+            label.id = marker_array.markers[-1].id + 1
+            label.type = Marker.TEXT_VIEW_FACING
+            label.action = Marker.ADD
+
+            label.text = voted_gesture or ""
+
+            label.pose.position.x = 0.4 if key == "right" else -0.4
+            label.pose.position.y = -0.4
+            label.pose.position.z = 0
+
+            label.pose.orientation.x = 0
+            label.pose.orientation.y = 0
+            label.pose.orientation.z = 0
+            label.pose.orientation.w = 1
+
+            label.scale.z = 0.05
+
+            label.color.a = 1.0
+            label.color.g = 0.6
+            label.color.r = 0.2
+
+            marker_array.markers.append(label)
+
+        self.vis_pub.publish(marker_array)
+
 
 
 
@@ -304,27 +464,46 @@ class GestureNode:
 ### ENTRY POINT ###
 ###################
 if __name__ == "__main__":
-    GESTURES = [
-        GestureTemplate(
-            name="good", hand="either",
-            finger_states={"index": FingerState.FOLDED, "middle": FingerState.FOLDED,
-                           "ring":  FingerState.FOLDED, "pinky":  FingerState.FOLDED,
-                           "thumb": FingerState.OPEN},
-                thumb_dir=Direction.UP 
-        ),
-        GestureTemplate(
-            name="follow-me", hand="either",
-            finger_states={"index": FingerState.OPEN, "middle": FingerState.OPEN,
-                            "ring": FingerState.OPEN, "pinky": FingerState.OPEN},
-            knuckle_dir=Direction.UP, palm_dir=Direction.AWAY, motion=Direction.AWAY
-        ),
-        GestureTemplate(
-            name="stop", hand="either",
-            finger_states={"index": FingerState.OPEN, "middle": FingerState.OPEN,
-                            "ring": FingerState.OPEN, "pinky": FingerState.OPEN},
-            knuckle_dir=Direction.UP, palm_dir=Direction.TOWARDS
-        ),
-    ]
+    # GESTURES = [
+    #     GestureTemplate(
+    #         name="good", hand="either",
+    #         finger_states={"index": FingerState.FOLDED, "middle": FingerState.FOLDED,
+    #                        "ring":  FingerState.FOLDED, "pinky":  FingerState.FOLDED,
+    #                        "thumb": FingerState.OPEN},
+    #             thumb_dir=Direction.UP
+    #     ),
+    #     GestureTemplate(
+    #         name="follow-me", hand="either",
+    #         finger_states={"index": FingerState.OPEN, "middle": FingerState.OPEN,
+    #                         "ring": FingerState.OPEN, "pinky": FingerState.OPEN},
+    #         knuckle_dir=Direction.UP, palm_dir=Direction.AWAY, motion=Direction.AWAY
+    #     ),
+    #     GestureTemplate(
+    #         name="stop", hand="either",
+    #         finger_states={"index": FingerState.FOLDED, "middle": FingerState.FOLDED,
+    #                         "ring": FingerState.FOLDED, "pinky": FingerState.FOLDED},
+    #         knuckle_dir=Direction.UP, palm_dir=Direction.TOWARDS, motion=Direction.STATIC
+    #     ),
+    #     GestureTemplate(
+    #         name="move-back", hand="either",
+    #         finger_states={"index": FingerState.OPEN, "middle": FingerState.OPEN,
+    #                         "ring": FingerState.OPEN, "pinky": FingerState.OPEN},
+    #         knuckle_dir=Direction.UP, palm_dir=Direction.TOWARDS, motion=Direction.TOWARDS
+    #     ),
+    #     GestureTemplate(
+    #         name="move-forward", hand="either",
+    #         finger_states={"index": FingerState.OPEN, "middle": FingerState.OPEN,
+    #                         "ring": FingerState.OPEN, "pinky": FingerState.OPEN},
+    #         knuckle_dir=Direction.UP, palm_dir=Direction.TOWARDS, motion=Direction.AWAY
+    #     ),
+    # ]
 
-    node = GestureNode(GESTURES)
+    rospack = rospkg.RosPack()
+    package_root = rospack.get_path("person_follower")
+
+    gestures = GestureTemplate.from_json(package_root + "/scripts/gestures.json")
+
+    print(gestures)
+
+    node = GestureNode(gestures)
     rospy.spin()
